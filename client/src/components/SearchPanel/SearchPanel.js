9be4fd7648b1ff34d286c20114f7ce9beb6337e1
Guillaume Paugam <guipaugam@icloud.com>
2018-07-09 15:18:33 -0500

    Integrated the search panel

--- client/src/components/SearchPanel/SearchPanel.js
+++ client/src/components/SearchPanel/SearchPanel.js
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
-import "./Home.css"
+import "./SearchPanel.css"
 
-const SearchPanel = () => {
+const SearchPanel = props => (
+    <div className="row">
+        <div className="col-sm-12">
+        <br />
+            <div className="panel panel-primary">
+                <div className="panel-heading">
+                    <h3 className="panel-title">
+                        <strong>
+                        <i className="fa  fa-list-alt"></i> Search Parameters</strong>
+                    </h3>
+                </div>
+                <div className="panel-body">
+                    <form role="form">
 
-}
+                        <div className="form-group">
+                            <label htmlFor="search">Search Term:</label>
+                            <input type="text" className="form-control" id="search-term" />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="pwd">Number of Records to Retrieve:</label>
+                            <select className="form-control" id="article-count">
+                                <option value="1">1</option>
+                                <option value="5" selected>5</option>
+                                <option value="10">10</option>
+                            </select>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="start-year">Start Year (Optional):</label>
+                            <input type="text" className="form-control" id="start-year" />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="end-year">End Year (Optional):</label>
+                            <input type="text" className="form-control" id="end-year" />
+                        </div>
+
+                        <button type="submit" className="btn btn-default" id="run-search">
+                            <i className="fa fa-search"></i> Search</button>
+                        <button type="button" className="btn btn-default" id="clear-all">
+                            <i className="fa fa-trash"></i> Clear Results</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
 export default SearchPanel;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,26 +1,14 @@
 import React, { Component } from "react";
+import API from "../../utils/NYT_API";
+import SearchPanel from "../../components/SearchPanel"
 import "./Home.css"
 
 class Home extends Component {
     render () {
         return (
         <div>
-            {/* navbar */}
-            <br/>
-            <div className="container">
-                <h1 className="text-center">NYT Articles Scrubber</h1>
-                <br/>
-                <div className="row">
-                    <button className="btn btn-danger btn-lg">Search</button>
-                </div>
-                <div className="row">
-                    <button className="btn btn-danger btn-lg">Results</button>
-                </div>
-                <div className="row">
-                    <button className="btn btn-danger btn-lg">Saved Articles</button>
-                </div>
-
-            </div>
+            <SearchPanel>
+            </SearchPanel>
         </div>
         )}
 }
